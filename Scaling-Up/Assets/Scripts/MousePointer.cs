5861ca29cd3dfd68e5d8c5d8a94cc6e8f3ca7799
enigmaDevelopments <enigmarezac@gmail.com>
2024-08-17 16:03:32 -0400

    add shooting arm to player

--- Scaling-Up/Assets/Scripts/MousePointer.cs
+++ Scaling-Up/Assets/Scripts/MousePointer.cs
@@ -11,12 +11,14 @@ public class MousePointer : MonoBehaviour
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 dir = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
         transform.up = dir;
-        Debug.Log(transform.rotation.eulerAngles.z);
-        Vector3 playerLocalScale = player.localScale;
-        if (transform.rotation.eulerAngles.z > 10 && transform.rotation.eulerAngles.z < 170)
-            playerLocalScale.x = -1f; 
-        else if (transform.rotation.eulerAngles.z < 350 && transform.rotation.eulerAngles.z > 190)
-            playerLocalScale.x = 1f;
-        player.localScale = playerLocalScale;
+        if (player != null)
+        {
+            Vector3 playerLocalScale = player.localScale;
+            if (transform.rotation.eulerAngles.z > 10 && transform.rotation.eulerAngles.z < 170)
+                playerLocalScale.x = -1f;
+            else if (transform.rotation.eulerAngles.z < 350 && transform.rotation.eulerAngles.z > 190)
+                playerLocalScale.x = 1f;
+            player.localScale = playerLocalScale;
+        }
     }
 }
