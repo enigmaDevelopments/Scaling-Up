5d5c617c3974ea048fc981f9a1ed63fb89ac4eef
enigmaDevelopments <enigmarezac@gmail.com>
2024-08-19 10:06:55 -0400

    move flipping from mouse pointer script to player movment script

--- Scaling-Up/Assets/Scripts/MousePointer.cs
+++ Scaling-Up/Assets/Scripts/MousePointer.cs
@@ -5,20 +5,10 @@ using UnityEngine;
 
 public class MousePointer : MonoBehaviour
 {
-    public Transform player;
     void Update()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 dir = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
         transform.up = dir;
-        if (player != null)
-        {
-            Vector3 playerLocalScale = player.localScale;
-            if (transform.rotation.eulerAngles.z > 10 && transform.rotation.eulerAngles.z < 170)
-                playerLocalScale.x = -1f;
-            else if (transform.rotation.eulerAngles.z < 350 && transform.rotation.eulerAngles.z > 190)
-                playerLocalScale.x = 1f;
-            player.localScale = playerLocalScale;
-        }
     }
 }
--- Scaling-Up/Assets/Scripts/PlayerMovment.cs
+++ Scaling-Up/Assets/Scripts/PlayerMovment.cs
@@ -5,77 +5,87 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-    public class PlayerMovment : MonoBehaviour
+public class PlayerMovment : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator animator;
+    public Transform groundCheck;
+    public Transform groundCheck2;
+    public Transform pivot;
+    public LayerMask groundLayer;
+    private float horizantal;
+    public float speed = 8f;
+    public float jump = 16f;
+    public float coyoteTime = .1f;
+    private bool notJumped = false;
+    private float timeFromGround = 100f;
+
+    void Update()
     {
-        public Rigidbody2D rb;
-        public Animator animator;
-        public Transform groundCheck;
-        public Transform groundCheck2;
-        public LayerMask groundLayer;
-        private float horizantal;
-        public float speed = 8f;
-        public float jump = 16f;
-        public float coyoteTime = .1f;
-        private bool notJumped = false;
-        private float timeFromGround = 100f;
-
-        void Update()
+        horizantal = Input.GetAxisRaw("Horizontal") * speed;
+        #region kyote timer
+        if (trueGrounded())
         {
-            horizantal = Input.GetAxisRaw("Horizontal") * speed;
-            #region kyote timer
-            if (trueGrounded())
-            {
-                notJumped = true;
-                timeFromGround = 0;
-            }
-            else
-                timeFromGround += Time.deltaTime;
-            #endregion
-            #region jump
-            if (Input.GetButtonDown("Jump") && Grounded())
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jump);
-                notJumped = false;
-            }
-            if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
-            {
-                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * .5f);
-            }
-            #endregion
-            animator.SetBool("isJumping", !trueGrounded());
-            animator.SetFloat("X Velocity",Mathf.Abs(rb.velocity.x));
-            animator.SetFloat("Y Velocity",rb.velocity.y);
-    }
-
-        void FixedUpdate()
-        {
-            rb.velocity = new Vector2(horizantal, rb.velocity.y);
+            notJumped = true;
+            timeFromGround = 0;
         }
-
-        #region find groundedness
-        public bool Grounded()
+        else
+            timeFromGround += Time.deltaTime;
+        #endregion
+        #region jump
+        if (Input.GetButtonDown("Jump") && Grounded())
         {
-            return notJumped && timeFromGround <= coyoteTime;
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+            notJumped = false;
         }
-        public bool trueGrounded()
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
-            return checkGround(groundLayer);
-        }
-        public bool checkGround(LayerMask layers)
-        {
-            return checkArea(groundCheck, groundCheck2, layers, gameObject);
-        }
-        public GameObject[] checkGroundAll(LayerMask layers)
-        {
-            return checkAreaAll(groundCheck, groundCheck2, layers, gameObject);
-        }
-        public static bool checkArea(Transform check, Transform check2, LayerMask layers, GameObject self)
-        {
-            return checkAreaAll(check, check2, layers, self).Count() != 0;
-        }
-        public static GameObject[] checkAreaAll(Transform check, Transform check2, LayerMask layers, GameObject self)
-        {
-            return (from col in Physics2D.OverlapAreaAll(check.position, check2.position, layers) where col.gameObject != self select col.gameObject).ToArray();
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * .5f);
         }
         #endregion
+        #region flip
+        Vector3 playerLocalScale = transform.localScale;
+        if (pivot.rotation.eulerAngles.z > 10 && pivot.rotation.eulerAngles.z < 170)
+            playerLocalScale.x = -Mathf.Abs(playerLocalScale.x);
+        else if (pivot.rotation.eulerAngles.z < 350 && pivot.rotation.eulerAngles.z > 190)
+            playerLocalScale.x = Mathf.Abs(playerLocalScale.x);
+        transform.localScale = playerLocalScale;
+        #endregion
+        #region animation correction
+        animator.SetBool("isJumping", !trueGrounded());
+        animator.SetFloat("X Velocity",Mathf.Abs(rb.velocity.x));
+        animator.SetFloat("Y Velocity",rb.velocity.y);
+        #endregion
     }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizantal, rb.velocity.y);
+    }
+
+    #region find groundedness
+    public bool Grounded()
+    {
+        return notJumped && timeFromGround <= coyoteTime;
+    }
+    public bool trueGrounded()
+    {
+        return checkGround(groundLayer);
+    }
+    public bool checkGround(LayerMask layers)
+    {
+        return checkArea(groundCheck, groundCheck2, layers, gameObject);
+    }
+    public GameObject[] checkGroundAll(LayerMask layers)
+    {
+        return checkAreaAll(groundCheck, groundCheck2, layers, gameObject);
+    }
+    public static bool checkArea(Transform check, Transform check2, LayerMask layers, GameObject self)
+    {
+        return checkAreaAll(check, check2, layers, self).Count() != 0;
+    }
+    public static GameObject[] checkAreaAll(Transform check, Transform check2, LayerMask layers, GameObject self)
+    {
+        return (from col in Physics2D.OverlapAreaAll(check.position, check2.position, layers) where col.gameObject != self select col.gameObject).ToArray();
+    }
+    #endregion
+}
